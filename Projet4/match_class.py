15c4180e34db39c2d9236b2a60b34d4fdb19d16a
AdrienB-OC <87777427+AdrienB-OC@users.noreply.github.com>
2021-11-07 20:26:45 +0900

    Delete Projet4 directory

--- Projet4/match_class.py
+++
@@ -1,6 +0,0 @@
-class Match:
-    def __init__(self, player1, player2, result):
-        self.player1 = player1
-        self.player2 = player2
-        self.result = result
-
--- Projet4/round_class.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-
-class Round:
-    def __init__(self, round_number):
-        self.start_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        self.end_time = ''
-        self.round_number = round_number
-        self.match_list = []
